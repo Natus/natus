a5228eddf55ef896640406bc678de308768f61c7
Nathaniel McCallum <nathaniel@natemccallum.com>
2011-04-01 20:24:27 -0400

    add script test

--- natus/tests/cxx_require.cc
+++ natus/tests/cxx_require.cc
@@ -34,13 +34,21 @@ int doTest(Engine& engine, Value& global) {
 
 	// Load the internal module again
 	Value moduleb = req.require("__internal__");
-	assert(!modulea.isException());
-	assert(modulea.isObject());
-	assert(modulea.get("misc").toLong() == 0x1234);
+	assert(!moduleb.isException());
+	assert(moduleb.isObject());
+	assert(moduleb.get("misc").toLong() == 0x1234);
 
 	// Check to make sure that the underlying values refer to the same object
 	assert(!modulea.set("test", 17L).isException());
 	assert(moduleb.get("test").toLong() == 17);
+
+	// Load a script file
+	Value scriptmod = req.require("script");
+	assert(!scriptmod.isException());
+	assert(scriptmod.isObject());
+	assert(scriptmod.get("number").toLong() == 115);
+	assert(scriptmod.get("string").toStringUTF8() == "hello world");
+
 
 	// Cleanup
 	return 0;
