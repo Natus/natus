b2c85eccf78333f9473af0ac702d922dd888fcbc
Nathaniel McCallum <nathaniel@natemccallum.com>
2011-04-13 12:59:03 -0400

    fix runjstest to properly setup the NATUS_PATH

--- tests/runjstest.c
+++ tests/runjstest.c
@@ -3,16 +3,15 @@
 
 #include "test.h"
 
-#define CMDPREFIX NATUSBIN " -C natus.require.path=\"['" MODULEDIR "']\" -e '"
-
 int onEngine(const char *engine, int argc, const char **argv) {
 	char cmd[PATH_MAX];
-	strncpy(cmd, CMDPREFIX, PATH_MAX);
+	strncpy(cmd, NATUSBIN " -e '", PATH_MAX);
 	strncat(cmd, engine, PATH_MAX - strlen(cmd));
 	strncat(cmd, "' ", PATH_MAX - strlen(cmd));
 	if (argc > 1)
 		strncat(cmd, argv[1], PATH_MAX - strlen(cmd));
 
+	putenv("NATUS_PATH=" MODULEDIR);
 	printf("%s\n", cmd);
 	int es = system(cmd);
 	return WEXITSTATUS(es);
