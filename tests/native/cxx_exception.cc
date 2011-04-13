bf10db579d43da17e141003d05e8a13d91fc8e3c
Nathaniel McCallum <nathaniel@natemccallum.com>
2011-04-13 11:44:43 -0400

    add cxx_execption test

---
+++ tests/native/cxx_exception.cc
@@ -0,0 +1,10 @@
+#include "test.hpp"
+
+int doTest(Engine& engine, Value& global) {
+	Value exc = throwException(global, "Error", "Foo %d", 23);
+	assert(exc.to<UTF8>() == "Error: Foo 23");
+
+	exc = throwException(global, "Error", 7, "Foo %d", 23);
+	assert(exc.to<UTF8>() == "Error: 7: Foo 23");
+	return 0;
+}
