52a3c8b4e431419306bcac6f48199e9b1248f572
Nathaniel McCallum <nathaniel@natemccallum.com>
2011-04-14 11:40:04 -0400

    make object test more thourough

--- tests/native/cxx_object.cc
+++ tests/native/cxx_object.cc
@@ -8,46 +8,34 @@ public:
 
 	virtual Value del(Value& obj, Value& name) {
 		assert(obj.isObject());
-		if (name.to<UTF8>() == "exc")
-			return obj.newString("error").toException();
-		if (name.to<UTF8>() == "oom")
-			return NULL;
-		if (name.isNumber())
-			obj.setPrivate("test", (void*) name.to<size_t>());
-		else
-			obj.setPrivate("test", (void*) name.to<UTF8>().length());
-		return obj.newBool(true);
+		assert(name.getType() == obj.getPrivate<size_t>("type"));
+		return obj.getPrivate<Value>("retval");
 	}
 
 	virtual Value get(Value& obj, Value& name) {
 		assert(obj.isObject());
-		if (name.to<UTF8>() == "exc")
-			return obj.newString("error").toException();
-		if (name.to<UTF8>() == "oom")
-			return NULL;
-		return name;
+		assert(name.getType() == obj.getPrivate<size_t>("type"));
+		return obj.getPrivate<Value>("retval");
 	}
 
 	virtual Value set(Value& obj, Value& name, Value& value) {
 		assert(obj.isObject());
-		if (name.to<UTF8>() == "exc")
-			return obj.newString("error").toException();
-		if (name.to<UTF8>() == "oom")
-			return NULL;
-		obj.setPrivate("test", (void*) value.to<size_t>());
-		return obj.newBool(true);
+		assert(name.getType() == obj.getPrivate<size_t>("type"));
+		return obj.getPrivate<Value>("retval");
 	}
 
 	virtual Value enumerate(Value& obj) {
 		assert(obj.isObject());
-		return obj.getPrivate<Value>("enumerate");
+		return obj.getPrivate<Value>("retval");
 	}
 
-	virtual Value call(Value& obj, Value& ths,  Value& args) {
+	virtual Value call(Value& obj, Value& ths, Value& args) {
 		assert(obj.isObject());
 		assert(args.isArray());
-		assert(args.get("length").to<int>() > 0);
-		return args.get(0);
+		assert(ths.isGlobal() || ths.isUndefined());
+		if (args.get("length").to<int>() == 0)
+			return obj.getPrivate<Value>("retval");
+		return args[0];
 	}
 };
 
@@ -56,65 +44,146 @@ int doTest(Engine& engine, Value& global) {
 	Value x = global.get("x");
 	assert(x.isObject());
 
-	// Delete
-	assert(!x.del(7).isException());
-	assert(7 == x.getPrivate<long>("test"));
+	//// Check for bypass
+	x.setPrivate("retval", NULL); // Ensure NULL
+	// Number, native
+	assert(x.setPrivate("type", (void*) Value::TypeNumber));
+	assert(!x.set(0, 17).isException());
+	assert(!x.get(0).isException());
+	assert( x.get(0).to<int>() == 17);
+	assert(!x.del(0).isException());
+	assert( x.get(0).isUndefined());
+	// Number, eval
+	assert(!global.evaluate("x[0] = 17;").isException());
+	assert(!global.evaluate("x[0];").isException());
+	assert( global.evaluate("x[0];").to<int>() == 17);
+	assert(!global.evaluate("delete x[0];").isException());
+	assert( global.evaluate("x[0];").isUndefined());
+	// String, native
+	assert(x.setPrivate("type", (void*) Value::TypeString));
+	assert(!x.set("foo", 17).isException());
+	assert(!x.get("foo").isException());
+	assert( x.get("foo").to<int>() == 17);
 	assert(!x.del("foo").isException());
-	assert(strlen("foo") == x.getPrivate<size_t>("test"));
+	assert( x.get("foo").isUndefined());
+	// String, eval
+	assert(!global.evaluate("x['foo'] = 17;").isException());
+	assert(!global.evaluate("x['foo'];").isException());
+	assert( global.evaluate("x['foo'];").to<int>() == 17);
+	assert(!global.evaluate("delete x['foo'];").isException());
+	assert( global.evaluate("x['foo'];").isUndefined());
 
-	// Get
-	Value y = x.get("foo");
-	assert(y.isString());
-	assert(y.to<UTF8>() == "foo");
-	y = x.get(7);
-	assert(y.isNumber());
-	assert(7 == y.to<int>());
+	//// Check for exception
+	assert(x.setPrivate("retval", global.newString("error").toException()));
+	// Number, native
+	assert(x.setPrivate("type", (void*) Value::TypeNumber));
+	assert(x.del(0).isException());
+	assert(x.get(0).isException());
+	assert(x.set(0, 0).isException());
+	// Number, eval
+	assert(global.evaluate("delete x[0];").isException());
+	assert(global.evaluate("x[0];").isException());
+	assert(global.evaluate("x[0] = 0;").isException());
+	// String, native
+	assert(x.setPrivate("type", (void*) Value::TypeString));
+	assert(x.del("foo").isException());
+	assert(x.get("foo").isException());
+	assert(x.set("foo", 0).isException());
+	// String, eval
+	assert(global.evaluate("delete x['foo'];").isException());
+	assert(global.evaluate("x['foo'];").isException());
+	assert(global.evaluate("x['foo'] = 0;").isException());
 
-	// Set
-	assert(!x.set("foo", x.newNumber(7)).isException());
-	assert(7 == x.getPrivate<long>("test"));
-	assert(!x.set(12345, x.newNumber(14)).isException());
-	assert(14 == x.getPrivate<long>("test"));
+	//// Check for intercept
+	assert(x.setPrivate("retval", global.newBool(true)));
+	// Number, native
+	assert(x.setPrivate("type", (void*) Value::TypeNumber));
+	assert(!x.del(0).isException());
+	assert(x.get(0).isBool());
+	assert(x.get(0).to<bool>());
+	assert(!x.set(0, 0).isException());
+	// Number, eval
+	assert(!global.evaluate("delete x[0];").isException());
+	assert(global.evaluate("x[0];").isBool());
+	assert(global.evaluate("x[0];").to<bool>());
+	assert(!global.evaluate("x[0] = 0;").isException());
+	// String, native
+	assert(x.setPrivate("type", (void*) Value::TypeString));
+	assert(!x.del("foo").isException());
+	assert(x.get("foo").isBool());
+	assert(x.get("foo").to<bool>());
+	assert(!x.set("foo", 0).isException());
+	// String, eval
+	assert(!global.evaluate("delete x['foo'];").isException());
+	assert(global.evaluate("x['foo'];").isBool());
+	assert(global.evaluate("x['foo'];").to<bool>());
+	assert(!global.evaluate("x['foo'] = 0;").isException());
 
+	//// Check for successful calls
 	// Call from C++
-	y = global.call("x", global.newArrayBuilder(global.newNumber(123)));
+	Value y = global.call("x", global.newArrayBuilder(123));
 	assert(!y.isException());
-	assert(123 == y.to<int>());
-
+	assert(y.isNumber());
+	assert(y.to<int>() == 123);
 	// New from C++
 	y = global.callNew("x", global.newArrayBuilder(global.newObject()));
 	assert(!y.isException());
 	assert(y.isObject());
-
 	// Call from JS
 	y = global.evaluate("x(123);");
 	assert(!y.isException());
-	assert(123 == y.to<int>());
-
+	assert(y.isNumber());
+	assert(y.to<int>() == 123);
 	// New from JS
 	y = global.evaluate("new x({});");
 	assert(!y.isException());
 	assert(y.isObject());
 
+	//// Check for exception calls
+	assert(x.setPrivate("retval", global.newString("error").toException()));
+	// Call from C++
+	y = global.call("x");
+	assert(y.isString());
+	assert(y.isException());
+	// New from C++
+	y = global.callNew("x");
+	assert(y.isString());
+	assert(y.isException());
+	// Call from JS
+	y = global.evaluate("x();");
+	assert(y.isString());
+	assert(y.isException());
+	// New from JS
+	y = global.evaluate("new x();");
+	assert(y.isString());
+	assert(y.isException());
+
+	//// Check for NULL calls
+	assert(x.setPrivate("retval", NULL)); // Ensure NULL
+	// Call from C++
+	y = global.call("x");
+	assert(y.isUndefined());
+	assert(y.isException());
+	// New from C++
+	y = global.callNew("x");
+	assert(y.isUndefined());
+	assert(y.isException());
+	// Call from JS
+	y = global.evaluate("x();");
+	assert(y.isUndefined());
+	assert(y.isException());
+	// New from JS
+	y = global.evaluate("new x();");
+	assert(y.isUndefined());
+	assert(y.isException());
+
 	// Enumerate
-	x.setPrivate("enumerate", x.newArrayBuilder(x.newNumber(5)).newArrayBuilder(x.newNumber(10)));
+	assert(x.setPrivate("retval", x.newArrayBuilder(x.newNumber(5)).newArrayBuilder(x.newNumber(10))));
 	y = x.enumerate();
 	assert(y.isArray());
 	assert(y.get("length").to<int>() == 2);
 	assert(y.get(0).to<int>() == 5);
 	assert(y.get(1).to<int>() == 10);
-
-	// Now we check for fault states
-	/*assert(!x.del("exc"));
-	//assert(!x.del("oom"));
-	assert(x.get("exc").isException());
-	//assert(x.get("oom").isOOM());
-	assert(!x.set("exc", x.newNumber(5)));
-	//assert(!x.set("oom", x.newNumber(5)));
-	x.setPrivate("enumerate", x.newString("error").toException());
-	assert(x.enumerate().isException());
-	x.setPrivate("enumerate", NULL);
-	assert(x.enumerate().isOOM());*/
 
 	// Cleanup
 	assert(!global.del("x").isException());
